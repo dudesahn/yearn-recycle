907f5dfe6c0e0019e2e7796b96b9ae57727051cf
dudesahn <dudesahn@gmail.com>
2021-03-06 20:37:00 -0500

    Update for BUSD

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,11 +18,11 @@ def coins(interface):
         interface.ERC20('0x6B175474E89094C44Da98b954EedeAC495271d0F'),
         interface.ERC20('0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'),
         interface.USDT('0xdAC17F958D2ee523a2206206994597C13D831ec7'),
-        interface.ERC20('0x0000000000085d4780B73119b644AE5ecd22b376'),
-        interface.ERC20('0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8'),
+        interface.ERC20('0x4Fabb145d64652a948d72533023f6E7A623C7C53'),
+        interface.ERC20('0x3B3Ac5386837Dc563660FB6a0937DFAa5924333B'),
     ]
 
 
 @pytest.fixture(scope='module')
-def yusd(interface):
-    return interface.ERC20('0x5dbcF33D8c2E976c6b560249878e6F1491Bca25c')
+def ybusd(interface):
+    return interface.ERC20('0x2994529C0652D127b7842094103715ec5299bBed')
